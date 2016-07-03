719bf98b0b6d16b34f83dbb775265338d300fa42
shavvn <shawn.li.xjtu@gmail.com>
2016-07-03 13:45:21 -0400

    got to generate commands and the cross product of all params

--- sst_simu.py
+++ sst_simu.py
@@ -2,36 +2,52 @@
 explore what interfaces should look like in this file
 """
 
-import json
+import utils
+import itertools
 
 
-class SSTSimu(object):
+class SSTSimulator(object):
     def __init__(self, config_file=""):
         self.configs = {}
         if config_file:
             with open(config_file) as configs:
-                self.configs = json.load(configs, object_hook=_byteify)
-        print self.configs
+                self.configs = utils.json_to_dict(configs)
 
+    def mpi_cmd_gen(self, program_cmd):
+        cmd = self.configs["sim_opts"]["mpi_opts"]["mpi_exe"]
+        n_threads = self.configs["sim_opts"]["mpi_opts"]["n"]
+        n_threads = int(n_threads)
+        cmd = "%s -n %d %s"%(cmd, n_threads, program_cmd)
+        return cmd
 
-def _byteify(data, ignore_dicts=False):
-    # if this is a unicode string, return its string representation
-    if isinstance(data, unicode):
-        return data.encode('utf-8')
-    # if this is a list of values, return list of byteified values
-    if isinstance(data, list):
-        return [_byteify(item, ignore_dicts=True) for item in data]
-    # if this is a dictionary, return dictionary of byteified keys and values
-    # but only if we haven't already byteified it
-    if isinstance(data, dict) and not ignore_dicts:
-        return {
-            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
-            for key, value in data.iteritems()
-        }
-    # if it's anything else, return it in its original form
-    return data
+    def assemble_command(self):
+        cmd = "sst %s"%self.configs["sim_opts"]["other_opts"]
+        if self.configs["sim_opts"]["mpi"]:
+            cmd = self.mpi_cmd_gen(cmd)
+        return cmd
+
+    def get_all_params(self):
+        """
+        get all the combinations of params based on config["model_params"]
+        (basically a cross product
+        :return: a list of dict objects, each dict is an unique param set
+        """
+        params = self.configs["model_params"]
+        param_list = itertools.product(*params.values())
+        param_dict_list = []
+        for v in param_list:
+            param_dict_list.append(dict(zip(params, v)))
+        # now param_dict_list has all combinations of params
+        return param_dict_list
+
+    def run(self):
+        cmd = self.assemble_command()
+        params = self.get_all_params()
+        print cmd
+        for p in params:
+            print p
 
 
 if __name__ == "__main__":
-    sst_simu = SSTSimu("sst_example.json")
-
+    sst_simu = SSTSimulator("sst_example.json")
+    sst_simu.run()
