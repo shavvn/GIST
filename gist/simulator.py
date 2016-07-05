7d7374e169f49aa4894f12f71220e23b1289b7c2
shavvn <tig314@gmail.com>
2016-07-04 20:13:03 -0400

    move common facilities into Simulator class, still works

--- gist/simulator.py
+++ gist/simulator.py
@@ -1,26 +1,108 @@
 """
-It's gonna take a while to figure out the structure of this porject.
+It's gonna take a while to figure out the structure of this project.
 We will see...
 """
+import itertools
+import json
+import os
+import sys
+import tempfile
+import utils
+from subprocess import call
+
 
 class Simulation(object):
     """ Top Level Class
     Should be able to do something like:
-    simu = Simulation(simulator="sst", config="config.json"
+    sim = Simulation(simulator="sst", config="config.json"
                       output_dir="./examples/")
-    simu.run(mpi=true, mpi_opts="-n 4")
-    simu.analysis(output="results.csv")
-    simu.visual(format="png", output_dir="./examples/graphs")
+    sim.run(mpi=true, mpi_opts="-n 4")
+    sim.analysis(output="results.csv")
+    sim.visual(format="png", output_dir="./examples/graphs")
     """
+
 
 class Simulator(object):
     """
     Generic Simulator Class, should have the following interfaces:
-    Config / Params: the way that simluator takes input for different
+    Config / Params: the way that simulator takes input for different
                      configs or params
     Run Commands: run time commands, like the executable, mpi, debug, 
                   verbose, input/output dirs
     """
-    
-    
-    
+    def __init__(self, config_file=""):
+        self.configs = {}
+        self.cmd = ""
+        self.params = []
+        self.logger = utils.get_default_logger()
+        if config_file:
+            with open(config_file) as configs:
+                self.configs = utils.json_to_dict(configs)
+                self.cmd = self.assemble_command()
+                self.params = self.get_all_params()
+        if not self.configs:
+            self.logger.fatal("Cannot load config file!")
+            sys.exit(1)
+
+    def mpi_cmd_gen(self, program_cmd):
+        """ if support MPI then get an MPI prefix with basic options
+        :param program_cmd: stuff to attach to after mpi
+        :return: command string with MPI
+        """
+        cmd = self.configs["sim_opts"]["mpi_opts"]["mpi_exe"]
+        n_threads = self.configs["sim_opts"]["mpi_opts"]["n"]
+        n_threads = int(n_threads)
+        cmd = "%s -n %d %s" % (cmd, n_threads, program_cmd)
+        return cmd
+
+    def assemble_command(self):
+        """ assemble commands that will be called later
+        format is like [mpi stuff][simulator stuff][other]
+        could be overridden if necessary
+        :return: command string
+        """
+        cmd = self.configs["sim_opts"]["sim_exe"]
+        if self.configs["sim_opts"]["mpi"]:
+            cmd = self.mpi_cmd_gen(cmd)
+        cmd = self.add_specific_opts(cmd)
+        return cmd
+
+    def add_specific_opts(self, pre_cmd):
+        """ this handles ["other_opts"]
+        :param pre_cmd: the command before adding simulator specific opts
+        :return: complete command with simulator specific opts
+        """
+        self.logger.info("No simulator-specific opts!")
+        return pre_cmd
+
+    def get_all_params(self):
+        """
+        get all the combinations of params based on config["model_params"]
+        (basically a cross product
+        :return: a list of dict objects, each dict is an unique param set
+        """
+        params = self.configs["model_params"]
+        param_list = itertools.product(*params.values())
+        param_dict_list = []
+        for v in param_list:
+            param_dict_list.append(dict(zip(params, v)))
+        # now param_dict_list has all combinations of params
+        return param_dict_list
+
+    def run(self):
+        """ run simulation by calling the exe
+        this should also be simulator-specific, since they may have different
+        command line formats
+        :return: none
+        """
+        print self.cmd
+        counter = 0
+        for p in self.params:
+            tmp_fp = tempfile.NamedTemporaryFile(delete=False)
+            json.dump(p, tmp_fp)
+            tmp_fp.close()
+            cmd = self.cmd + " " + tmp_fp.name
+            self.logger.debug("calling: %s" % cmd)
+            call(cmd, shell=True)
+            os.remove(tmp_fp.name)
+            counter += 1
--- gist/sst_simu.py
+++ gist/sst_simu.py
@@ -2,44 +2,11 @@
 explore what interfaces should look like in this file
 """
 import utils
-import itertools
-import json
-import os
 import sys
-import tempfile
-from subprocess import call
+import simulator
 
 
-class SSTSimulator(object):
-    def __init__(self, config_file=""):
-        self.configs = {}
-        self.cmd = ""
-        self.params = []
-        self.logger = None
-        self.logger = utils.get_default_logger()
-        print self.logger.level
-        if config_file:
-            with open(config_file) as configs:
-                self.configs = utils.json_to_dict(configs)
-                self.cmd = self.assemble_command()
-                self.params = self.get_all_params()
-        if not self.configs:
-            self.logger.fatal("Cannot load config file!")
-            sys.exit(1)
-
-    def mpi_cmd_gen(self, program_cmd):
-        cmd = self.configs["sim_opts"]["mpi_opts"]["mpi_exe"]
-        n_threads = self.configs["sim_opts"]["mpi_opts"]["n"]
-        n_threads = int(n_threads)
-        cmd = "%s -n %d %s" % (cmd, n_threads, program_cmd)
-        return cmd
-
-    def assemble_command(self):
-        cmd = self.configs["sim_opts"]["sim_exe"]
-        if self.configs["sim_opts"]["mpi"]:
-            cmd = self.mpi_cmd_gen(cmd)
-        cmd = self.add_specific_opts(cmd)
-        return cmd
+class SSTSimulator(simulator.Simulator):
 
     def add_specific_opts(self, pre_cmd):
         """ this handles ["other_opts"]
@@ -50,38 +17,6 @@ class SSTSimulator(object):
         tgt = sst_opts["target_script"]
         cmd = pre_cmd + " " + tgt
         return cmd
-
-    def get_all_params(self):
-        """
-        get all the combinations of params based on config["model_params"]
-        (basically a cross product
-        :return: a list of dict objects, each dict is an unique param set
-        """
-        params = self.configs["model_params"]
-        param_list = itertools.product(*params.values())
-        param_dict_list = []
-        for v in param_list:
-            param_dict_list.append(dict(zip(params, v)))
-        # now param_dict_list has all combinations of params
-        return param_dict_list
-
-    def run(self):
-        """ run batch jobs
-        this should also be simulator-specific, since they may have different
-        command line formats
-        :return: none
-        """
-        print self.cmd
-        counter = 0
-        for p in self.params:
-            tmp_fp = tempfile.NamedTemporaryFile(delete=False)
-            json.dump(p, tmp_fp)
-            tmp_fp.close()
-            cmd = self.cmd + " " + tmp_fp.name
-            self.logger.debug("calling: %s" % cmd)
-            call(cmd, shell=True)
-            os.remove(tmp_fp.name)
-            counter += 1
 
 
 if __name__ == "__main__":
