d6af9c16a5d6417fa5956e4208d2ad5ae4c21e5f
shavvn <tig314@gmail.com>
2016-07-06 22:38:39 -0400

    add logger test, failed tho...

--- gist/tests/test_utils.py
+++ gist/tests/test_utils.py
@@ -8,6 +8,13 @@ class UtilTest(unittest.TestCase):
         d = utils.json_to_dict(fp)
         self.assertIsInstance(d, dict)
 
+    def test_logger_debug(self):
+        arg_parser = utils.ArgParser("-d")
+        logger = arg_parser.get_logger()
+        logger.debug("you should see this")
+        level = logger.level
+        self.assertEqual(level, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
