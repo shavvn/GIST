096f7e4f7539b3774dcaad32a057884c7a0c44bb
shavvn <tig314@gmail.com>
2016-07-20 14:02:49 -0400

    add test cases for compile output

--- gist/tests/test_sst.py
+++ gist/tests/test_sst.py
@@ -1,22 +1,43 @@
+import os
 import unittest
 from .. import sst_simu
 
 
 class SSTTest(unittest.TestCase):
+    def setUp(self):
+        summary_csv = "gist/tests/test_output/summary.csv"
+        if os.path.exists(summary_csv):
+            os.remove(summary_csv)
+            
     def test_init(self):
         sst = sst_simu.SSTSimulator("examples/sst_example.json")
-        self.assertTrue(sst.configs, "Config not found!")
+        self.assertTrue(sst.params, "params not found!")
 
     def test_cmd_gen(self):
         sst = sst_simu.SSTSimulator("examples/sst_example.json")
-        sst.configs["sim_opts"]["mpi"] = True
+        sst.sim_opts["mpi"] = True
         cmd = sst.assemble_command()
         self.assertIn("mpirun", cmd)
 
     def test_params_enum(self):
         sst = sst_simu.SSTSimulator("examples/sst_example.json")
-        param_list = sst.get_all_params()
-        self.assertEqual(len(param_list), 4)
-
+        self.assertEqual(len(sst.params), 4)
+    
+    def test_get_ext_time(self):
+        test_str = "Simulation is complete, simulated time: 12.3 ms"
+        t_correct = 12300
+        t_get = sst_simu.SSTSimulator._get_exe_time(test_str)
+        self.assertEqual(t_get, t_correct)
+       
+    def test_add_metri_to_header(self):
+        header = ["foo", "bar"]
+        sst = sst_simu.SSTSimulator("examples/sst_example.json")
+        header_after = sst._add_metrics_to_header(header,
+                        sst.stats)
+    
+    def test_compile_output(self):
+        sst_simu.SSTSimulator.compile_output("gist/tests/test_output")
+        
 if __name__ == '__main__':
     unittest.main()
+
