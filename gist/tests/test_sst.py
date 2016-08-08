e3fe8e0a24cbf0324a5c154cc656fc5991922535
shavvn <tig314@gmail.com>
2016-08-08 16:48:58 -0400

    add test case for compile ember output

--- gist/tests/test_sst.py
+++ gist/tests/test_sst.py
@@ -9,7 +9,11 @@ class SSTTest(unittest.TestCase):
         sst = sst_simu.SSTSimulator("examples/sst_example.json")
         self.assertTrue(sst.param_list, "params not found!")
 
-       
+    def test_compile_ember_output(self):
+        sst_simu.compile_ember_output("gist/tests/test_output/ember/5k.log",
+                                      "gist/tests/test_output/ember/")
+        self.assertTrue(os.path.exists("gist/tests/test_output/ember/summary.csv"))
+
 if __name__ == '__main__':
     unittest.main()
 
