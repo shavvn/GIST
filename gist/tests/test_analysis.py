eb20622e109e2d6d41315c078793b4d9a40eacc1
shavvn <tig314@gmail.com>
2016-08-11 16:45:34 -0400

    add one test case for analysis

---
+++ gist/tests/test_analysis.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+import pandas as pd
+from .. import analysis
+
+
+class AnalysisTest(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read_csv("gist/tests/ember_output/test_topo_sep.csv")
+
+    def test_separate_topo(self):
+        new_df = analysis.separate_topos(self.df)
+        self.assertTrue(not any(topo == "torus" for topo in new_df["topo"]))
+        self.assertTrue(not any(topo == "fattree" for topo in new_df["topo"]))
+
+    
