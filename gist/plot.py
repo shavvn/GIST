292392729ce0790124532aeb45079b24c953b4be
shavvn <tig314@gmail.com>
2016-09-13 22:39:14 -0400

    bar function sorta works, but still need finer grained control

--- gist/plot.py
+++ gist/plot.py
@@ -80,35 +80,53 @@ def lines(params, fig_format="png", output_dir="."):
 
 
 def bars(params, fig_format="png", output_dir="."):
-    fig, ax = plt.subplot(1, 1)
-    cnt = 0
-    # TODO if data is more than one dimension, plot as grouped
     if len(params["x"]) == 0:
         return
     else:
-        x_type = type(params["x"][0])
+        fig, ax = plt.subplots(1, 1)
         x_ticks = []
         x_ticklabels = []
-        # TODO figure out this 4 situation things...
-        if x_type == str:
-            x_tick = 0
-            for x in params["x"]:
-                for x_label in x:
-                    if x_label not in x_ticklabels:
-                        x_ticklabels.append(x_label)
-                        x_ticks.append(x_tick)
-                        x_tick += 1
-                    else:
-                        pass
-        else:
-            # dont plot ticks since too many...
-            pass
+        tick = 0
+        for x in params["x"]:
+            for x_label in x:
+                if x_label not in x_ticklabels:
+                    x_ticklabels.append(x_label)
+                    x_ticks.append(tick)
+                    tick += 1
         if len(params["x"]) > 1:
-            # plot as grouped bars
-            pass
+            bars_cnt = len(params["legends"])  # should = len(params["x])
+            # 4 bars share 1 unit (cm)
+            dist = bars_cnt / 4 + 1
+            x_ticks = [x*dist for x in x_ticks]
+            bar_width = float(1.0) / float(bars_cnt + 1)
+            offset = 0.0
+            for x_labels, y in zip(params["x"], params["y"]):
+                _ticks = [x_ticklabels.index(x_label) for x_label in x_labels]
+                _ticks = map(lambda x: x*dist, _ticks)
+                x_pos = [x + offset for x in _ticks]
+                ax.bar(x_pos, height=y, width=bar_width)
+                offset += bar_width
+            ax.set_xticks(x_ticks)
+            ax.set_xticklabels(x_ticklabels, ha="center", rotation="vertical")
         else:
             ax.set_xticks(x_ticks)
-            ax.set_ticklabels(x_ticklabels, ha="center", rotation="vertical")
-            ax.bar(x_ticks, height=y, align="center", edgecolor="none")
-            pass
+            ax.set_ticklabels(x_ticklabels,
+                              ha="center",
+                              rotation="vertical")
+            ax.bar(x_ticks, height=params["y"], align="center", edgecolor="none")
+        ax.set_title(params["title"])
+        ax.legend(params["legends"], loc="best")
+        ax.set_xlabel(params["x_label"])
+        ax.set_ylabel(params["y_label"])
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        output_name = os.path.join(output_dir, params["save_name"])
+        if fig_format == "png":
+            output_name += ".png"
+        elif fig_format == "pdf":
+            output_name += ".pdf"
+        else:
+            exit("invalid output format!")
+        fig.savefig(output_name, format=fig_format)
+        plt.close(fig)
 
